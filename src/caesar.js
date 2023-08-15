78fc8652320ad08911f2768d0fe38cda76276c6e
Clarisse <clarisse421@berkeley.edu>
2023-08-15 15:46:08 -0400

    finished caesar and polybius

--- src/caesar.js
+++ src/caesar.js
@@ -7,8 +7,72 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    // PARAMETERS: 
+      //input - text to be encoded or decoded
+      //shift - how much letter is "shifted by". "+" = right, "-" = left
+      //encode - whether to encode or decode message; default is true
+      //if encode is false, make the shift negative (var shiftValue *= -1)
+      
+      //if no shift is any of these(not defined, < -25, > 25), return false
+      if (shift === 0 ||  shift < -25 || shift > 25 ) return false;
+        
+      //create a lookup variable to find the index of a given letter in the alphabet 
+      const lookup = "abcdefghijklmnopqrstuvwxyz";
+      //create a var to hold new string
+      let output = "";
+      
+      //ignore capitals (.lowerCase())
+      const lowerCaseInput = input.toLowerCase();
+      const formula = encode ? shift : shift * -1;
+
+      //iterate through each of the letters in the input
+      for (let i=0; i < lowerCaseInput.length; i++) {
+        let originalChar = lowerCaseInput.charAt(i);
+        let newChar;
+        
+        //maintain spaces in translation(& other nonalphabetic symbols). aka stays the same
+        if (!lookup.includes(originalChar)) {
+            newChar = originalChar;
+    
+        } else {
+            // console.log("originalChar",originalChar);
+            //find the alphabet index of the letter using lookup, store in variable j
+                //index of
+            const originalCharIndex = lookup.indexOf(originalChar);
+            // set var newIndex = j + shiftz
+            //var shift = 
+                  //if encode is false, then take the negative of shift. Otherwise leave it alone. This encompasses this logic: 
+                  //if shift is positive and encode is true, go right ("Thinkful", 3, True) //> 'wklqnixo'
+                  //if shift is positive and encode is false, go left ("wklqnixo", 3, false) //> 'thinkful'
+                  //if shift is negative and encode is true, go left ("Thinkful", -3, True) //> 'qefkhcri'
+                  //if shift is negative and encode is false, go right ("qefkhcri, -3, False") //> 'thinkful'
+            // identify the char at the new index in lookup and add to output
+            let newCharIndex = originalCharIndex + formula;
+
+            //if alphabet shifted "off" wrap around
+            //if char goes below a, do this:
+            if (newCharIndex < 0) {
+              // newIndex + 25
+              newCharIndex += 26;
+
+            } 
+            //if char goes below z, do this:
+            if (newCharIndex > 25) {
+                //newIndex - 25
+                newCharIndex -= 26;
+            }
+            
+            //text.charAt
+            newChar = lookup.charAt(newCharIndex);
+        }
+        output += newChar;
+    }
+
+    return output;
+
   }
+
+  console.log(caesar("zebra magazine", 3))
 
   return {
     caesar,
--- src/polybius.js
+++ src/polybius.js
@@ -7,8 +7,82 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    // PARAMS: input, encode
+    	//string - inputted text
+    		// assume that there will no symbols. only spaces and letters
+    	//boolean(defaults true) - indicates whether to encode or decode the message
+
+	//if decoding, check if the length of the input string is even. Else return false
+	
+  const noSpacesArray = input.split(" ");
+  const noSpacesString = noSpacesArray.join('');
+  
+	if ((!encode) && (noSpacesString.length % 2 != 0)) return false;
+
+
+	//create a lookup for encoding letters to numbers
+	const lookup = {a: "11", b: "21", c: "31", d: "41", e: "51", f: "12", g: "22", h: "32", i: "42", j: "42", k: "52", l: "13", m: "23", n: "33", o: "43", p: "53", q: "41", r: "24", s: "34", t: "44", u: "54", v: "15", w: "25", x: "35", y: "45",z: "55"}
+
+	//create a reverseLookup for decoding numbers to letters
+	const reverseLookup = {11: "a", 21: "b", 31: "c", 41: "d", 51: "e", 12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k", 13: "l", 23: "m", 33: "n", 43: "o", 53: "p", 14: "q", 24: "r", 34: "s", 44: "t", 54: "u", 15: "v", 25: "w", 35:"x", 45: "y", 55: "z"}
+
+	//output variable = ""
+  let output = "";
+	
+	//IF ENCODING
+	if (encode) {
+		//for each letter in the input
+		for (let i = 0; i < input.length; i++) {
+			const currChar = input.charAt(i).toLowerCase();
+			//if its a space, then just add a space to the output
+			if (currChar === " ") {
+				output += " "
+			
+			// else, use the lookup o find the corresponding numbers
+			} else {
+				const numString = lookup[currChar]
+        output += numString;
+			}
+		}
+				 
+	//ELSE DECODING
+	} else {
+		
+			//for loop that iterates by 2 
+      for (let i = 0; i < input.length; i += 2 ) {
+        let firstChar = input.charAt(i);
+        let secondChar = input.charAt(i + 1)
+
+        //if there's a space. 
+          //for firstChar, then just make secondChar the first char, adjust i
+          //for second char, then skip the space and make the next char first char, adjust i
+        if (firstChar === " ") {
+          output += " "
+          i -= 1;
+          
+        } 
+        else {
+          const lookupString = firstChar + secondChar
+        const letterCorr = reverseLookup[lookupString];
+				output += letterCorr;
+        } 
+      }
+	}
+
+	//return output
+		//when encoding, your output should still be as string
+	return output;
+
+
   }
+
+  // console.log("test1: ", polybius("thinkful"),"4432423352125413");
+  // console.log("test2: ", polybius("Hello world"), '3251131343 2543241341');
+  
+  // console.log("test3: ", polybius("3251131343 2543241341", false), "hello world" );
+  // console.log("test4: ",polybius("4432423352125413", false), "th(i/j)nkful");
+  
+  // console.log("testThinkful1:", polybius("message"));
 
   return {
     polybius,

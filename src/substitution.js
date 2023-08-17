9397125d309d4cb3638bf5454db6fe8b09bb1b6f
Clarisse <clarisse421@berkeley.edu>
2023-08-17 10:59:58 -0400

    substitution function complete

--- src/substitution.js
+++ src/substitution.js
@@ -11,26 +11,24 @@ const substitutionModule = (function () {
       //input(string) - text to be encoded or decoded
       //alphabet(string)- substitution alphabet
       //encode(boolean) - whether to encode or decode
-
-    if (alphabet.length != 26) return false;
+  
+    if (!alphabet || alphabet.length != 26) return false;
     //alphabet.length MUST be 25 or return false
     
     //all letters in alphabet MUST be unique or return false
     //create an object. iterate through all of the elements in the alphabet.
     const regAlphabet = "abcdefghijklmnopqrstuvwxyz";
-    const subAlphabetLookup = {};
-    const subAlphabetReverseLookup = {}
+    const subAlphabetLookup = {} //
     
-    //for each elem in alphabet, check if it exists in object. 
+    //for each elem in alphabet, check if it exists in the object. 
     for (let i = 0; i < alphabet.length; i++) {
       //If it exists, return false, 
       const currChar = alphabet.charAt(i);
-      if (subAlphabetReverseLookup[currChar]) {
+      if (subAlphabetLookup[currChar]) {
         return false;
         //if not add it to the object
       } else {
-        subAlphabetReverseLookup[currChar] = i;
-        subAlphabetLookup[i] = currChar;
+        subAlphabetLookup[currChar] = i;
       }
     }
     
@@ -40,8 +38,8 @@ const substitutionModule = (function () {
       //.tolowercase
 
       // for each elem in the input
-      for (let e = 0; e < input.length; e ++) {
-        const currChar = input.charAt(e);
+      for (let j = 0; j < input.length; j ++) {
+        const currChar = input.charAt(j);
         //IF its a space
         if (currChar === " ") {
           //add space to the output
@@ -53,7 +51,7 @@ const substitutionModule = (function () {
           const regAlphIndex = regAlphabet.indexOf(currChar.toLowerCase());
 
           // find the subchar using the charAt(foundIndex) in the subAlphabetLookup
-          const subchar = subAlphabetLookup[regAlphIndex];
+          const subchar = alphabet.charAt(regAlphIndex);
 
           // add sub char to output (output += subchar)
           output += subchar
@@ -63,12 +61,22 @@ const substitutionModule = (function () {
       //DECODING
     } else {
       //.tolowercase
-
-      // for each elem in the input
-      //IF its a space
-      //add space to the output
-      //ELSE
-      // find the indexOf the currChar using the subAlphabetLookup
+        // for each elem in the input
+        for (let j = 0; j < input.length; j++) {
+          //IF its a space
+          const currChar = input.charAt(j)
+          if (currChar === " ") {
+            //add space to the output
+            output += " ";
+          
+          //ELSE
+          } else {
+            // find the regular alphabet char of currChar using the subAlphabetLookup
+            const subCharIndex = subAlphabetLookup[currChar.toLowerCase()]
+            const regAlphChar = regAlphabet.charAt(subCharIndex);
+            output += regAlphChar;
+        }
+      }
       // given the index, use charAt(foundIndex) on regAlphabet to find its sub char
       // add sub char to output (output += subchar)
       
@@ -80,12 +88,6 @@ const substitutionModule = (function () {
     //spaces should be maintained throughout
     //capital letters can be ignored
   }
-
-  console.log(substitution("thinkful", "short")); //> false
-  console.log(substitution("thinkful", "abcabcabcabcabcabcabcabcyz")); //> false
-
-  console.log(substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev"), 'jrufscpw'); //> 'jrufscpw'
-  console.log(substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev"), 'elp xhm xf mbymwwmfj dne')
 
 
   return {
